3d2a1540d4fa916e5d4706167a185f0c3684c127
João Pedro <pedro-202080@hotmail.com>
2019-06-11 20:58:06 -0300

    Adcionar

--- salveViagens/src/main/java/com/projeto/salveViagens/controllers/EstadoController.java
+++ salveViagens/src/main/java/com/projeto/salveViagens/controllers/EstadoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.projeto.salveViagens.models.Estado;
@@ -17,8 +18,15 @@ public class EstadoController {
 	
 	@GetMapping("/listaEstado")
 	public ModelAndView lista() {
-		ModelAndView mv = new ModelAndView("estado");
+		ModelAndView mv = new ModelAndView("/estado");
 		List<Estado> estado = repository.findAll();
+		mv.addObject("estado", estado);
+		return mv;
+	}
+	
+	@PostMapping("/adcionarEstado")
+	public ModelAndView add(Estado estado) {
+		ModelAndView mv = new ModelAndView("/addestado");
 		mv.addObject("estado", estado);
 		return mv;
 	}
