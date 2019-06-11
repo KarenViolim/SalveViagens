08978539bbff24c1e616e493df4c95863213d547
João Pedro <pedro-202080@hotmail.com>
2019-06-11 20:51:22 -0300

    Lista do Estado Feita

--- salveViagens/src/main/java/com/projeto/salveViagens/controllers/EstadoController.java
+++ salveViagens/src/main/java/com/projeto/salveViagens/controllers/EstadoController.java
@@ -1,13 +1,26 @@
 package com.projeto.salveViagens.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.projeto.salveViagens.models.Estado;
 import com.projeto.salveViagens.repository.EstadoRepository;
 
 @Controller
 public class EstadoController {
 	@Autowired
 	public EstadoRepository repository;
+	
+	@GetMapping("/listaEstado")
+	public ModelAndView lista() {
+		ModelAndView mv = new ModelAndView("estado");
+		List<Estado> estado = repository.findAll();
+		mv.addObject("estado", estado);
+		return mv;
+	}
 
 }
