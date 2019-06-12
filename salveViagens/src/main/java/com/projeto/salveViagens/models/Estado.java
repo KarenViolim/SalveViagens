e5e12c7ac6da2708a8bb015194a34cb9bbe93376
mazute <leo.mazute@gmail.com>
2019-06-11 21:37:05 -0300

    q

--- salveViagens/src/main/java/com/projeto/salveViagens/models/Estado.java
+++ salveViagens/src/main/java/com/projeto/salveViagens/models/Estado.java
@@ -8,10 +8,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-
 public class Estado implements Serializable {
-
 	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private long id;
