ca11fbcc057624731f9f153be00df2069e9ff7c5
KarenViolim <karen.violim2011@gmail.com>
2019-06-12 20:32:25 -0300

    Iniciando Projeto

---
+++ salveViagens/src/main/java/com/projeto/salveViagens/SalveViagensApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.salveViagens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalveViagensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalveViagensApplication.class, args);
+	}
+
+}
