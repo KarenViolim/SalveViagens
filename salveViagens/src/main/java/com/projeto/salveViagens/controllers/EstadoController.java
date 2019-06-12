fb276a0748a72cb10f61404847584ef417cfeb11
João Pedro <pedro-202080@hotmail.com>
2019-06-11 21:52:49 -0300

    Controller Estado Terminado

--- salveViagens/src/main/java/com/projeto/salveViagens/controllers/EstadoController.java
+++ salveViagens/src/main/java/com/projeto/salveViagens/controllers/EstadoController.java
@@ -1,10 +1,15 @@
 package com.projeto.salveViagens.controllers;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -30,8 +35,27 @@ public class EstadoController {
 		mv.addObject("estado", estado);
 		return mv;
 	}
+	@GetMapping("/editarProd/{id}")
+	public ModelAndView editar(@PathVariable("id") Long id){
+		Optional<Estado> estado = repository.findById(id);
+		Estado e = estado.get();
+		return add(e);
+		
+	}
+	@GetMapping("/removerProd/{id}")
+	public ModelAndView remover(@PathVariable("id") Long id){
+		Optional<Estado> estado = repository.findById(id);
+		Estado e = estado.get();
+		repository.delete(e);
+		return lista();
+		
+	}
 	@PostMapping("/salvarEstado")
-	public ModelAndView salvar() {
+	public ModelAndView salvar(@Valid Estado estado, BindingResult result ) {
+		if(result.hasErrors()) {
+			
+		}
+		repository.saveAndFlush(estado);
 		return lista();
 	}
 
