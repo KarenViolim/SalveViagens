42e236e8a23758185e4b9b3caf23f5934549d57e
mazute <leo.mazute@gmail.com>
2019-06-12 22:15:18 -0300

    controller

--- salveViagens/src/main/java/com/projeto/salveViagens/controllers/CidadeController.java
+++ salveViagens/src/main/java/com/projeto/salveViagens/controllers/CidadeController.java
@@ -1,8 +1,11 @@
 package com.projeto.salveViagens.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -34,6 +37,20 @@ public class CidadeController {
 		return mv;
 	}
 	
+	@GetMapping("/editarCidade/{id}")
+	public ModelAndView editar(@PathVariable("id") long id) {
+		Optional<Cidade> op = repositoryCidade.findById(id);
+		Cidade cid = op.get();
+		return add(cid);
+	}
+	
+	@GetMapping("/removerCidade/{id}")
+	public ModelAndView remover(@PathVariable("id") long id) {
+		Optional<Cidade> op = repositoryCidade.findById(id);
+		Cidade cid = op.get();
+		repositoryCidade.delete(cid);
+		return listar();
+	}
 	
 	@PostMapping("/salvarCidade")
 	public ModelAndView salvar(Cidade cidade) {
