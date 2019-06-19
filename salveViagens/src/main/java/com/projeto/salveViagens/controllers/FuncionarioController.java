c5baf4bc0523482ba8bd99d868881bc5c55f7681
Aluno <Aluno@172.30.1.10>
2019-06-18 21:52:12 -0300

    arrumado funcionario

--- salveViagens/src/main/java/com/projeto/salveViagens/controllers/FuncionarioController.java
+++ salveViagens/src/main/java/com/projeto/salveViagens/controllers/FuncionarioController.java
@@ -3,6 +3,7 @@ package com.projeto.salveViagens.controllers;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,7 +12,7 @@ import org.springframework.web.servlet.ModelAndView;
 import com.projeto.salveViagens.models.Funcionario;
 import com.projeto.salveViagens.repository.CidadeRepository;
 import com.projeto.salveViagens.repository.FuncionarioRepository;
-
+@Controller
 public class FuncionarioController {
 	@Autowired
 	private FuncionarioRepository repositoryFuncionario;
