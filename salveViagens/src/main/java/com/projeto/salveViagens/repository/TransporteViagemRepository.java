090467c6347c6962c004dcd788f099dc9d6d0cf5
naldinho-ho <naldinho-ho@hotmail.com>
2019-06-18 21:26:11 -0300

    TransViagem Repository

---
+++ salveViagens/src/main/java/com/projeto/salveViagens/repository/TransporteViagemRepository.java
@@ -0,0 +1,9 @@
+package com.projeto.salveViagens.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projeto.salveViagens.models.TransporteViagem;
+
+public interface TransporteViagemRepository extends JpaRepository<TransporteViagem, Long>{
+
+}
